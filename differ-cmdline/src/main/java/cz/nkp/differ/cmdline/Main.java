805afe20731a52eceeb704578a6d47f7823671b1
Vaclav Rosecky <xrosecky@gmail.com>
2012-12-12 20:51:23 +0100

    stub for differ-cmdline added

--- differ-cmdline/src/main/java/cz/nkp/differ/cmdline/Main.java
+++ differ-cmdline/src/main/java/cz/nkp/differ/cmdline/Main.java
@@ -1,11 +1,19 @@
 package cz.nkp.differ.cmdline;
 
-/**
- *
- * @author xrosecky
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import cz.nkp.differ.compare.io.ImageProcessor;
+import cz.nkp.differ.compare.io.ImageProcessorResult;
+import java.io.File;
+
 public class Main {
 
-    
-
+    public static void main(String[] args) throws Exception {
+	ApplicationContext context =
+		new ClassPathXmlApplicationContext(new String[]{"appCtx-differ-cmdline.xml"});
+	ImageProcessor processor = (ImageProcessor) context.getBean("imageProcessor");
+	File file = new File(args[0]);
+	ImageProcessorResult result = processor.processImage(file);
+	System.out.println(String.format("%s: %sx%s", file.getAbsolutePath(), result.getHeight(), result.getWidth()));
+    }
 }
