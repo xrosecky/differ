afe1c5006eb0bfb9e5ce685ef25b722987c3973d
Vaclav Rosecky <xrosecky@gmail.com>
2013-01-04 23:02:40 +0100

    output serialized XML

--- differ-cmdline/src/main/java/cz/nkp/differ/cmdline/Main.java
+++ differ-cmdline/src/main/java/cz/nkp/differ/cmdline/Main.java
@@ -2,9 +2,12 @@ package cz.nkp.differ.cmdline;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import cz.nkp.differ.compare.io.ImageProcessor;
 import cz.nkp.differ.compare.io.ImageProcessorResult;
+import cz.nkp.differ.compare.io.SerializableImageProcessorResult;
 import java.io.File;
+import javax.xml.transform.stream.StreamResult;
 
 public class Main {
 
@@ -12,8 +15,11 @@ public class Main {
 	ApplicationContext context =
 		new ClassPathXmlApplicationContext(new String[]{"appCtx-differ-cmdline.xml"});
 	ImageProcessor processor = (ImageProcessor) context.getBean("imageProcessor");
+	Jaxb2Marshaller marshaller = (Jaxb2Marshaller) context.getBean("jaxb2Marshaller");
 	File file = new File(args[0]);
 	ImageProcessorResult result = processor.processImage(file);
-	System.out.println(String.format("%s: %sx%s", file.getAbsolutePath(), result.getHeight(), result.getWidth()));
+	SerializableImageProcessorResult resultForSerialization = SerializableImageProcessorResult.create(result, false);
+	StreamResult streamResult = new StreamResult(System.out);
+	marshaller.marshal(resultForSerialization, streamResult);
     }
 }
