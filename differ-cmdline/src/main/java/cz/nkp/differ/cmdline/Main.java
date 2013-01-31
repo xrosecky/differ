aed65ce33b5ba26b9aaebfb310718cf3c50c0df8
Vaclav Rosecky <xrosecky@gmail.com>
2013-01-31 20:15:31 +0100

    cmdline uses SerializableImageProcessorResults

--- differ-cmdline/src/main/java/cz/nkp/differ/cmdline/Main.java
+++ differ-cmdline/src/main/java/cz/nkp/differ/cmdline/Main.java
@@ -1,13 +1,16 @@
 package cz.nkp.differ.cmdline;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import cz.nkp.differ.compare.io.ImageProcessor;
 import cz.nkp.differ.compare.io.ImageProcessorResult;
 import cz.nkp.differ.compare.io.SerializableImageProcessorResult;
+import cz.nkp.differ.compare.io.SerializableImageProcessorResults;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import javax.xml.transform.stream.StreamResult;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 public class Main {
 
@@ -19,8 +22,12 @@ public class Main {
 	File file1 = new File(args[0]);
 	File file2 = new File(args[1]);
 	ImageProcessorResult[] results = processor.processImages(file1, file2);
-	SerializableImageProcessorResult resultForSerialization = SerializableImageProcessorResult.create(results[2], false);
+        List<SerializableImageProcessorResult> resultsForSerialization = new ArrayList<SerializableImageProcessorResult>();
+        for (ImageProcessorResult result : results) {
+            resultsForSerialization.add(SerializableImageProcessorResult.create(result, false));
+        }
+        SerializableImageProcessorResults serializableResults = new SerializableImageProcessorResults(resultsForSerialization);
 	StreamResult streamResult = new StreamResult(System.out);
-	marshaller.marshal(resultForSerialization, streamResult);
+	marshaller.marshal(serializableResults, streamResult);
     }
 }
