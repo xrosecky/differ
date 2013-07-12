452433d15e483506fda6f3c2cf4dbe3f2a956cde
Jonatan Svensson <jonatansve@gmail.com>
2013-07-12 17:41:10 +0200

    Added first tests, and updated recognized properties with new properties according to test failures

--- differ-cmdline/src/test/java/cz/nkp/differ/cmdline/JpylyzerUnitTest.java
+++ differ-cmdline/src/test/java/cz/nkp/differ/cmdline/JpylyzerUnitTest.java
@@ -14,8 +14,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.LinkedHashMap;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import javax.annotation.Resource;
 
 /**
  * Created with IntelliJ IDEA.
@@ -40,6 +43,51 @@ public class JpylyzerUnitTest {
         byte[] stdout = readFile("../docs/examples/images_01/14/output-jpylyzer.raw");
         List<ResultTransformer.Entry> transformedData = jpylyzerMetadataTransformer.transform(stdout,null);
         assertNotNull(transformedData);
+
+        /**
+         * Compare transformedData with list of
+         * manual input of significant properties in image14Test01RecognizedProperties
+         * Fails if a property is transformed but is yet not mapped.
+         */
+
+        ArrayList recognizedProperties = (ArrayList) image14Test01.get("image14Test01RecognizedProperties");
+        assertNotNull(recognizedProperties);
+        for(Entry e: transformedData){
+            assertTrue("Testing that transformed property is recognized: "+ e.getKey(),recognizedProperties.contains(e.getKey()));
+        }
+
+
+        /**
+         * Go through each entry in transformedData,
+         * Look for the key in:
+         * identificationProperties/validationProperties/characterizationProperties,
+         * Assert that the value is identical.
+         */
+
+        LinkedHashMap l = (LinkedHashMap) image14Test01.get("image14SignificantProperties");
+        LinkedHashMap lh1 = (LinkedHashMap) l.get("identificationProperties");
+        LinkedHashMap lh2 = (LinkedHashMap) l.get("validationProperties");
+        LinkedHashMap lh3 = (LinkedHashMap) l.get("characterizationProperties");
+
+        String s;
+
+        for(Entry e: transformedData){
+            s= (String)lh1.get(e.getKey());
+            if(s==null) {
+                s= (String)lh2.get(e.getKey());
+                if(s==null){
+                    s= (String)lh3.get(e.getKey());
+                }
+            }  // If s is null here, then the entry is missing in manual data
+
+            System.out.println("Testing: "+e.getKey()+ " with: "+ s);
+
+            if(s!= null){
+                System.out.println("Checking equality: "+ e.getValue() + " with: "+ s);
+                assertEquals("Testing: "+e.getKey(), e.getValue(), s);
+            }
+            s=null;
+        }
     }
 
 
