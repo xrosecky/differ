295036712b0c0d53ffb9d96136a98b50b877d61b
Thomas <tommytruax@gmail.com>
2013-07-14 22:55:16 -0500

    updated caching mechanics
    cache now supports hard references in addition to soft references

--- differ-webapp/src/main/java/cz/nkp/differ/cache/LRUCache.java
+++ differ-webapp/src/main/java/cz/nkp/differ/cache/LRUCache.java
@@ -6,57 +6,101 @@ import java.util.Map;
 
 
 /**
- * LRU (Least-Recently-Used) Cache that is capable of persistently storing a certain number of
- * objects, based on how frequently they are accessed. Thus, objects that are accessed most frequently
- * will remain in the cache, while objects that are accessed least frequently will be replaced as 
- * new objects are added.<br><br>
+ * LRU (Least-Recently-Used) Cache that is capable of persistently storing a certain number of objects, based on how
+ * frequently they are accessed. Thus, objects that are accessed most frequently will remain in the cache, while 
+ * objects that are accessed least frequently will be replaced as new objects are added.<br><br>
  * 
- * Objects added to the cache are added as {@link SoftReference}s to prevent any OutOfMemoryErrors from
- * occurring. This may be necessary in cases where cached objects might be very large or if the max size
- * of the cache is large.<br><br>
+ * Objects added to the cache are added as both hard (normal) references and {@link SoftReference}s. Since the 
+ * Garbage Collector may decide to evict {@link SoftReference}s prematurely, hard references are also used to protect
+ * the reliability of the cache. The most recently used items will always be moved to the hard cache, while the older
+ * items will be moved to the soft cache. Additionally, no items will be put into the soft cache until the hard
+ * cache has been filled. The ratio of hard cache items to soft cache items can be set via the
+ * <b>LRUCache(int maxSize, float hardSoftRatio)</b> constructor and the <b>resize(int maxSize, float hardSoftRatio)</b>
+ * method; this ratio must be between 0 and 1.0. The hard-soft ratio should be set to a relatively low value, especially
+ * if the max cache size is large, else there is an increased risk of triggering an {@link OutOfMemoryError}.<br><br>
  * 
- * All relevant methods are synchronized for thread-safety. This class also tracks cache hit data. 
- * If no argument is given to the constructor, a default cache size is used. 
+ * Example: If maxSize=100 and hardSoftRatio=0.2, the hard cache will store 20 items and the soft cache will store the 
+ * other 80 items.<br><br>
+ * 
+ * All relevant methods are synchronized for thread-safety. This class also tracks cache hit data. If no argument is 
+ * given to the constructor, a default cache size and hard-soft ratio are used. 
  * 
  * @author Thomas Truax
  */
 public class LRUCache<K, V> {
 	
-	private Map<K, SoftReference<V>> cache;
-	private int accessCnt = 0;
-	private int hitCnt = 0;
-	private int maxSize = 0;
-	private static int DEFAULT_SIZE = 20;
+	private Map<K, SoftReference<V>> softcache;
+	private Map<K, V> hardcache;
+    private float hardSoftRatio;
+	private int accessCnt;
+	private int hitCnt;
+	private int maxSize;
+    private int hardSize;
+    private int softSize;
+	private static int DEFAULT_SIZE = 50;
+    private static float DEFAULT_HARDSOFT_RATIO = 0.2f;
 
 	/**
-	 * No-argument constructor. Creates a new cache with a default size of 20.
+	 * No-argument constructor. Creates a new cache with a default size of 50 and default hard-soft ratio of 0.2.
 	 */
 	public LRUCache() {
-		this(DEFAULT_SIZE);
+            this(DEFAULT_SIZE);
 	}
 	
 	/**
-	 * Constructor that permits setting a defined size for the cache. Throws an {@link IllegalArgumentException}
-	 * if maxSize is less than or equal to zero.
+	 * Constructor that permits setting a defined maximum size for the cache while leaving the hard-soft ratio at the default
+	 * 0.2. Throws an {@link IllegalArgumentException} if maxSize is less than or equal to zero.
 	 * @param maxSize Maximum number of objects allowed in the cache
 	 */
-	public LRUCache(final int maxSize) {
-		if (maxSize <= 0) {
-			throw new IllegalArgumentException();
-		}
-		this.maxSize = maxSize;
-		build();
+    public LRUCache(final int maxSize) {
+        this(maxSize, DEFAULT_HARDSOFT_RATIO);
+    }
+        
+	/**
+	 * Constructor that permits setting a defined maximum size for the cache and its hard-soft ratio.
+	 * @param maxSize Maximum number of objects allowed in the cache; throws an {@link IllegalArgumentException}
+	 * if this value is less than 1
+	 * @param hardSoftRatio Ratio of hard cache items to soft cache items, represented as a float value; so 
+	 * if a value of 0.25 is passed, 1/4 of the maxSize will be stored in the hard cache; throws an
+	 * {@link IllegalArgumentException} if this value of less than 0 or greater than 1
+	 */
+	public LRUCache(final int maxSize, final float hardSoftRatio) {
+            if (maxSize <= 0 || hardSoftRatio < 0 || hardSoftRatio > 1) {
+            	throw new IllegalArgumentException();
+            }
+            this.maxSize = maxSize;
+            this.hardSoftRatio = hardSoftRatio;
+            hardSize = (int) (maxSize * hardSoftRatio);
+            softSize = maxSize - hardSize;
+            build();
 	}
 	
 	/*
 	 * Creates a new hash map object for the cache
 	 */
 	private void build() {
-		cache = new LinkedHashMap<K, SoftReference<V>>(maxSize, 1.1f, true) {
-			protected boolean removeEldestEntry(Map.Entry entry) {
-			    return size() > maxSize;
-			}
-		};
+        hardcache = new LinkedHashMap<K, V>(hardSize, 1.0f, true) {
+                    @Override
+                    protected boolean removeEldestEntry(Map.Entry<K, V> entry) {
+                        if (size() > hardSize) {
+                            if (softSize > 0) {
+                            	//if hard cache is full, push eldest entry to the soft cache
+                            	softcache.put(entry.getKey(), new SoftReference<V>(entry.getValue()));
+                            }
+                            return true;
+                        }
+                        return false;
+                    }
+        };
+        softcache = new LinkedHashMap<K, SoftReference<V>>(softSize, 1.0f, true) {
+					@Override
+					protected boolean removeEldestEntry(Map.Entry<K, SoftReference<V>> entry) {
+                        if (size() > softSize) {
+                            return true;
+                        }
+                        return false;
+					}
+        };
 	}
 	
 	/**
@@ -65,8 +109,8 @@ public class LRUCache<K, V> {
 	 * @param key - Key to map to the passed object
 	 * @param obj - Object to cache
 	 */
-	public synchronized void add(final K key, final V obj) {
-		cache.put(key, new SoftReference<V>(obj));
+	public synchronized void put(final K key, final V obj) {
+        hardcache.put(key, obj);
 	}
 
 	/**
@@ -75,13 +119,17 @@ public class LRUCache<K, V> {
 	 * @return the object that was removed; otherwise null if the key doesn't exist
 	 */
 	public synchronized V remove(final K key) {
-		hitCheck(key);
-		//must explicitly check for null here, otherwise a NullPointerException
-		//could potentially be thrown when trying to get() SoftReference
-		if (cache.containsKey(key)) {
-			return cache.remove(key).get();
-		}
-		return null;
+        if (hitCheck(key)) {
+            if (hardcache.containsKey(key)) {
+                return hardcache.remove(key);
+            }
+            //must explicitly check for null here, otherwise a NullPointerException
+            //could potentially be thrown when trying to get() SoftReference
+            if (softcache.get(key) != null) {
+                return softcache.remove(key).get();
+            }
+        }
+        return null;
 	}
 
 	/**
@@ -89,14 +137,18 @@ public class LRUCache<K, V> {
 	 * @param key - The key for the desired object
 	 * @return the object mapped to the passed key; otherwise null if the key doesn't exist
 	 */
-	public synchronized V get(K key) {
-		hitCheck(key);
-		//must explicitly check for null here, otherwise a NullPointerException
-		//could potentially be thrown when trying to get() SoftReference
-		if (cache.get(key) != null) {
-			return cache.get(key).get();
-		}
-		return null;
+	public synchronized V get(final K key) {
+        if (hitCheck(key)) {
+            if (hardcache.containsKey(key)) {
+                return hardcache.get(key);
+            }
+            //must explicitly check for null here, otherwise a NullPointerException
+            //could potentially be thrown when trying to get() SoftReference
+            if (softcache.get(key) != null) {
+                return softcache.get(key).get();
+            }
+        }
+        return null;
 	}
 
 	/**
@@ -104,54 +156,107 @@ public class LRUCache<K, V> {
 	 * @param key - The key to check
 	 * @return true if the key exists in the map, else returns false
 	 */
-	public synchronized boolean isCached(K key) {
-		hitCheck(key);
-		return cache.containsKey(key);
+	public synchronized boolean isCached(final K key) {
+        return hitCheck(key);
 	}
 
 	/*
-	 * Increments the access count for the cache, as well as the hit count if the key exists
+	 * Increments the access count for the cache, as well as the hit count if the key exists.
+	 * If a hit was successful and the item is in the soft cache, it is moved to the hard cache.
 	 */
-	private void hitCheck(K key) {
-		accessCnt++;
-		if (cache.containsKey(key)) {
-			hitCnt++;
-		}
+	private boolean hitCheck(final K key) {
+        accessCnt++;
+        if (hardcache.containsKey(key)) {
+            hitCnt++;
+            return true;
+        } else if (softcache.containsKey(key)) {
+        	hitCnt++;
+        	if (hardSize > 0) {
+        		V harden = softcache.remove(key).get();
+        		hardcache.put(key, harden);
+        	}
+        	return true;
+        }
+        return false;
 	}
 	
 	/**
-	 * Resizes the maximum size of the cache. Throws an {@link IllegalArgumentException}
-	 * if maxSize is less than or equal to zero.
-	 * @param maxSize - The new size for the cache (must be greater than zero)
+	 * Resizes the maximum size of the cache.
+	 * @param maxSize Maximum number of objects allowed in the cache; throws an {@link IllegalArgumentException}
+	 * if this value is less than 1
 	 */
-	public synchronized void resize(final int maxSize) {
-		if (maxSize <= 0) {
-			throw new IllegalArgumentException();
-		}
-		this.maxSize = maxSize;
-		Map<K, SoftReference<V>> newCache = new LinkedHashMap<K, SoftReference<V>>(this.maxSize, 1.1f, true) {
-			protected boolean removeEldestEntry(Map.Entry entry) {
-			    return size() > maxSize;
-			}
-		};
-		
-		// *if new maxSize is smaller than current map size(), we must do some extra work 
-		// since most-recently-used items are always at end of the list
-		
-		if (this.maxSize < cache.size()) {
-			int i = cache.size() - this.maxSize;
-			int j = 0;
-			for (Map.Entry<K, SoftReference<V>> entry : cache.entrySet()) {
-				j++;
+    public synchronized void resize(final int maxSize) {
+        resize(maxSize, hardSoftRatio);
+    }
+        
+	/**
+	 * Resizes the maximum size of the cache and adjusts its hard-soft ratio.
+	 * @param maxSize Maximum number of objects allowed in the cache; throws an {@link IllegalArgumentException}
+	 * if this value is less than 1
+	 * @param hardSoftRatio Ratio of hard cache items to soft cache items, represented as a float value; so 
+	 * if a value of 0.25 is passed, 1/4 of the maxSize will be stored in the hard cache; throws an
+	 * {@link IllegalArgumentException} if this value of less than 0 or greater than 1
+	 */
+	public synchronized void resize(final int maxSize, final float hardSoftRatio) {
+        if (maxSize <= 0 || hardSoftRatio < 0 || hardSoftRatio > 1) {
+            throw new IllegalArgumentException();
+        }
+        this.maxSize = maxSize;
+        this.hardSoftRatio = hardSoftRatio;
+        hardSize = (int) (maxSize * hardSoftRatio);
+        softSize = maxSize - hardSize;
+        Map<K, V> newHardCache = new LinkedHashMap<K, V>(hardSize, 1.0f, true) {
+                    @Override
+                    protected boolean removeEldestEntry(Map.Entry<K,V> entry) {
+                        if (size() > hardSize) {
+                        	if (softSize > 0) {
+                        		//if hard cache is full, push eldest entry to the soft cache
+                        		softcache.put(entry.getKey(), new SoftReference<V>(entry.getValue()));
+                        	}
+                            return true;
+                        }
+                        return false;
+                    }
+        };
+        Map<K, SoftReference<V>> newSoftCache = new LinkedHashMap<K, SoftReference<V>>(softSize, 1.0f, true) {
+                    @Override
+                    protected boolean removeEldestEntry(Map.Entry<K, SoftReference<V>> entry) {
+                        return size() > softSize;
+                    }
+        };
+	
+        // *if new hardSize/softSize is smaller than current hard/soft size(), we must
+        //do some extra work since most-recently-used items are always at end of the list
+
+        if (hardSize < hardcache.size()) {
+        	int i = hardcache.size() - hardSize;
+        	int j = 0;
+        	for (Map.Entry<K, V> entry : hardcache.entrySet()) {
+        		j++;
+        		if (j >= i) {
+        			newHardCache.put(entry.getKey(), entry.getValue());
+        		}
+        	}
+        } else {
+        	newHardCache.putAll(hardcache);
+        }
+	
+        hardcache = newHardCache;
+        
+        if (softSize < softcache.size()) {
+        	int i = softcache.size() - softSize;
+        	int j = 0;
+        	for (Map.Entry<K, SoftReference<V>> entry : softcache.entrySet()) {
+        		j++;
 				if (j >= i) {
-					newCache.put(entry.getKey(), entry.getValue());
+		            newSoftCache.put(entry.getKey(), entry.getValue());
 				}
-			}
-		} else {
-			newCache.putAll(cache);
-		}
-		
-		cache = newCache;
+        	}
+        } else {
+        	newSoftCache.putAll(softcache);
+        }
+
+        softcache = newSoftCache;
 	}
 	
 	/**
@@ -160,7 +265,7 @@ public class LRUCache<K, V> {
 	 * @return the number of objects in the cache
 	 */
 	public synchronized int getSize() {
-		return cache.size();
+        return hardcache.size() + softcache.size();
 	}
 	
 	/**
@@ -168,7 +273,15 @@ public class LRUCache<K, V> {
 	 * @return the current max size of the cache
 	 */
 	public synchronized int getMaxSize() {
-		return maxSize;
+        return maxSize;
+	}
+	
+	/**
+	 * Returns the value of the hard-soft ratio.
+	 * @return the hard-soft ratio represented as a float
+	 */
+	public synchronized float getHardSoftRatio() {
+		return hardSoftRatio;
 	}
 	
 	/**
@@ -176,7 +289,7 @@ public class LRUCache<K, V> {
 	 * @return true if cache size is equal to zero, else false
 	 */
 	public synchronized boolean isEmpty() {
-		return cache.size() == 0;
+        return hardcache.isEmpty() && softcache.isEmpty();
 	}
 	
 	/**
@@ -184,7 +297,7 @@ public class LRUCache<K, V> {
 	 * @return true if cache size is equal to the max size, else false
 	 */
 	public synchronized boolean isFull() {
-		return cache.size() == maxSize;
+        return getSize() == maxSize;
 	}
 	
 	/**
@@ -192,7 +305,7 @@ public class LRUCache<K, V> {
 	 * method instead.
 	 */
 	public synchronized void clear() {
-		build();
+        build();
 	}
 	
 	/**
@@ -200,9 +313,9 @@ public class LRUCache<K, V> {
 	 * the cache to its original state. 
 	 */
 	public synchronized void reset() {
-		build();
-		accessCnt = 0;
-		hitCnt = 0;
+        build();
+        accessCnt = 0;
+        hitCnt = 0;
 	}
 	
 	/**
@@ -212,7 +325,7 @@ public class LRUCache<K, V> {
 	 * @return the total number of accesses to the cache
 	 */
 	public synchronized int getAccessCount() {
-		return accessCnt;
+        return accessCnt;
 	}
 	
 	/**
@@ -221,7 +334,7 @@ public class LRUCache<K, V> {
 	 * @return the total number of successful hits to the cache
 	 */
 	public synchronized int getHitCount() {
-		return hitCnt;
+        return hitCnt;
 	}
 	
 	/**
@@ -230,6 +343,6 @@ public class LRUCache<K, V> {
 	 * @return the total number of misses to the cache
 	 */
 	public synchronized int getMissCount() {
-		return accessCnt - hitCnt;
+        return accessCnt - hitCnt;
 	}
 }
