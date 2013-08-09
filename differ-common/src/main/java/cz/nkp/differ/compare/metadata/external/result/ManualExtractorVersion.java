d06e4c35a74665a5867a72fd8539e9883064d6b0
Jonatan Svensson <jonatansve@gmail.com>
2013-08-09 16:59:01 +0200

    Changes to differ-common for manual extractor version

--- differ-common/src/main/java/cz/nkp/differ/compare/metadata/external/result/ManualExtractorVersion.java
+++ differ-common/src/main/java/cz/nkp/differ/compare/metadata/external/result/ManualExtractorVersion.java
@@ -6,21 +6,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created with IntelliJ IDEA.
- * User: stavel
- * Date: 1.3.13
- * Time: 19:40
+ * Sets manual value for extractor whenever the information is missing from raw output
+ * @author Jonatan Svensson
+ * @version 09/08/2013
  */
 public class ManualExtractorVersion implements MetadataResultTransformer {
+    private String version;
+
     public List<ResultTransformer.Entry> transform(List<ResultTransformer.Entry> metadataList){
-        List<ResultTransformer.Entry> toRemove = new ArrayList<ResultTransformer.Entry>();
-        for(ResultTransformer.Entry entry: metadataList){
-            String value=entry.getValue();
-            if( value == null || value.isEmpty() ){
-                toRemove.add(entry);
-            }
-        }
-        metadataList.removeAll(toRemove);
+        ResultTransformer.Entry e = new ResultTransformer.Entry();
+        e.setKey("Version of Extractor");
+        e.setValue(version);
+        metadataList.add(e);
         return metadataList;
+    }
+
+    public void setVersion(String value){
+        version=value;
+    }
+    public String getVersion(){
+        return version;
     }
 }
