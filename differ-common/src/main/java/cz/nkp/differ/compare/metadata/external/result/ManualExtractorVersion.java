885467865ea3fba017cf0a719f2ac7e43ebb15c1
Jonatan Svensson <jonatansve@gmail.com>
2013-08-09 16:57:12 +0200

    Fixed kdu_expand extractor version with new bean ManualExtractorVersion

---
+++ differ-common/src/main/java/cz/nkp/differ/compare/metadata/external/result/ManualExtractorVersion.java
@@ -0,0 +1,26 @@
+package cz.nkp.differ.compare.metadata.external.result;
+
+import cz.nkp.differ.compare.metadata.external.ResultTransformer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: stavel
+ * Date: 1.3.13
+ * Time: 19:40
+ */
+public class ManualExtractorVersion implements MetadataResultTransformer {
+    public List<ResultTransformer.Entry> transform(List<ResultTransformer.Entry> metadataList){
+        List<ResultTransformer.Entry> toRemove = new ArrayList<ResultTransformer.Entry>();
+        for(ResultTransformer.Entry entry: metadataList){
+            String value=entry.getValue();
+            if( value == null || value.isEmpty() ){
+                toRemove.add(entry);
+            }
+        }
+        metadataList.removeAll(toRemove);
+        return metadataList;
+    }
+}
