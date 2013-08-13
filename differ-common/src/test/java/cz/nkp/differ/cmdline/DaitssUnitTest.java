212b68026cc5105c0f16fe5e3e703e977592de06
Jonatan Svensson <jonatansve@gmail.com>
2013-08-13 12:02:16 +0200

    Added normalizers and changed extractors to correct SP

--- differ-common/src/test/java/cz/nkp/differ/cmdline/DaitssUnitTest.java
+++ differ-common/src/test/java/cz/nkp/differ/cmdline/DaitssUnitTest.java
@@ -18,7 +18,7 @@ import java.util.Map;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:fitsTestsCtx.xml"})
+@ContextConfiguration(locations = {"classpath:daitssTestsCtx.xml"})
 public class DaitssUnitTest {
     TestRunner t = new TestRunner();
     String thisExtractor = this.getClass().getSimpleName();
@@ -29,19 +29,20 @@ public class DaitssUnitTest {
     @Resource
     private LinkedHashMap<String, Object> extractorSignificantProperties;
     @Autowired
-    private ResultTransformer fitsMetadataTransformer;
+    private ResultTransformer daitssMetadataTransformer;
 
     Logger logger = LogManager.getLogger(DaitssUnitTest.class.getName());
 
     @Test
     public void testImage14() throws Exception {
-        byte[] stdout = TestHelper.readFile("../docs/examples/images_01/14/output-fits.raw");
-        List<ResultTransformer.Entry> transformedData = fitsMetadataTransformer.transform(stdout, null);
+        byte[] stdout = TestHelper.readFile("../docs/examples/images_01/14/output-daitss.raw");
+        List<ResultTransformer.Entry> transformedData = daitssMetadataTransformer.transform(stdout, null);
         ArrayList ignoredProperties = (ArrayList) image14Test01.get("ignoredSignificantProperties");
         ArrayList recognizedProperties = (ArrayList) image14Test01.get("recognizedSignificantProperties");
         LinkedHashMap<String, Object> specialProperties = (LinkedHashMap) image14Test01.get("specialSignificantProperties");
         assertNotNull(recognizedProperties);
         LinkedHashMap significantProperties = (LinkedHashMap) image14Test01.get("significantProperties");
+
         t.runStandardTests(transformedData,
                 recognizedProperties,
                 ignoredProperties,
