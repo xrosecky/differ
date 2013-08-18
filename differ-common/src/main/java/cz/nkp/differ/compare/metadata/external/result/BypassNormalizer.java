e9b92507847aac2bc51676846163853ca359a7b9
Jonatan Svensson <jonatansve@gmail.com>
2013-08-18 13:44:11 +0200

    Cleanup

--- differ-common/src/main/java/cz/nkp/differ/compare/metadata/external/result/BypassNormalizer.java
+++ differ-common/src/main/java/cz/nkp/differ/compare/metadata/external/result/BypassNormalizer.java
@@ -4,17 +4,12 @@ import cz.nkp.differ.compare.metadata.external.ResultEntryValueTransformer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
- * Created with IntelliJ IDEA.
- * User: stavel
- * Date: 13.2.13
- * Time: 20:09
- */
+*   Normalizer for "Coding bypass"
+ *  Value should be yes or no
+ *  @author Jonatan Svensson
+ *
+ *  */
 public class BypassNormalizer implements ResultEntryValueTransformer {
     Logger logger = LogManager.getLogger(BypassNormalizer.class.getName());
 
