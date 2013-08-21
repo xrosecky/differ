741d7c894d614831c321c1728ad08aff861603eb
Jonatan Svensson <jonatansve@gmail.com>
2013-08-21 13:14:59 +0200

    Implemented extractor timer - working correctly, now displayed as an extractor property in miliseconds.

--- differ-common/src/main/java/cz/nkp/differ/compare/metadata/external/ExtractorTimer.java
+++ differ-common/src/main/java/cz/nkp/differ/compare/metadata/external/ExtractorTimer.java
@@ -1,20 +1,40 @@
 package cz.nkp.differ.compare.metadata.external;
 
+import cz.nkp.differ.compare.metadata.ImageMetadata;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
+import java.util.ArrayList;
+
+/**@author Jonatan Svensson
+ * @date 21/08/2013
+ *
+ * Starts timer before all metadata extraction and stops right after.
+ * Saves the timer into the corresponding list.
+ * If there is no list computed for the extractor then do nothing.
+ */
 
 public class ExtractorTimer {
+    Logger logger = LogManager.getLogger(ExtractorTimer.class.getName());
 
     public Object startAndStopTimer(ProceedingJoinPoint pjp) throws Throwable {
         long startTime = System.nanoTime();
-         System.out.println(startTime);
-        Object retVal = pjp.proceed();
+        logger.debug("Starting timer, time is: "+startTime);
+        Object o = pjp.proceed();
         long endTime = System.nanoTime();
-        long duration = endTime - startTime;
-        System.out.println(duration);
+        long duration = (endTime - startTime)/1000000;
 
-        return retVal;
-
+        try{
+            ArrayList retVal = (ArrayList<ImageMetadata>) o;
+            ImageMetadata i= (ImageMetadata) retVal.get(0);
+            ImageMetadata timerProperty = new ImageMetadata("Elapsed Time of Extraction",duration,i.getSource());
+            timerProperty.setUnit("ms");
+            retVal.add(timerProperty);
+            return retVal;
+        }
+        catch(Exception e){
+            logger.error("Error: There is no element in metadata in order to fetch source name");
+        }
+        return o;
     }
 }
